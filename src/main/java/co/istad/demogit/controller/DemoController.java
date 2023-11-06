2bbedf10be064795483501ca27e382d1dc29f21c
Chan Chhaya <it.chhaya@gmail.com>
2023-11-06 19:32:31 +0700

    Modify /demo to response dynamic keyword

--- src/main/java/co/istad/demogit/controller/DemoController.java
+++ src/main/java/co/istad/demogit/controller/DemoController.java
@@ -1,12 +1,13 @@
 package co.istad.demogit.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
     @GetMapping("/demo")
-    public String demo() {
-        return "Hello Git";
+    public String demo(@RequestParam String keyword) {
+        return keyword;
     }
 }
