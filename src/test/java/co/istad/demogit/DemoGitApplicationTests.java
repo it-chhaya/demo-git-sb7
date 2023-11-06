576f311a586d00e29ad686b662aa58e97ee8ce6f
Chan Chhaya <it.chhaya@gmail.com>
2023-11-06 18:57:03 +0700

    My first initialize commit

---
+++ src/test/java/co/istad/demogit/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package co.istad.demogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
